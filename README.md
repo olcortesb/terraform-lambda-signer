885a945338e5207327f55bd8b05eeceede410d54
olcortesb <oscar.cortes@ackstorm.com>
2024-03-16 16:16:10 +0100

    add signer configurations

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # terraform-lambda-signer
 POC of AWS lambda Signer with terraform
+
+
+## Get Starter
+
+- Configure terraform 
+- Configure AWS credentials ([Link](https://gist.github.com/olcortesb/a471797eb1d45c54ad51d920b78aa664))
+- Create and configure bucket name 
+
+## Deploy
+
+```
+terraform plan
+terraform apply
+```
+
+## Folder structure
+
+```bash
+.
+├── data.tf
+├── lambda_role.tf
+├── lambda.tf
+├── LICENSE
+├── preferences.tf
+├── README.md
+├── src                # Lambda Folder
+│   └── getcost.py     # Lamba Code
+├── terraform.tfvars
+└── variables.tf
+```
+
+## What Is AWS Signer
+
+ref: https://docs.aws.amazon.com/signer/latest/developerguide/Welcome.html
+
+>AWS Signer is a fully managed code-signing service to ensure the trust and integrity of your code. Organizations validate code against a digital signature to confirm that the code is unaltered and from a trusted publisher. With AWS Signer, your security administrators have a single place to define your signing environment, including what AWS Identity and Access Management (IAM) role can sign code and in what Regions. AWS Signer manages the code-signing certificate's public and private keys, and enables central management of the code-signing lifecycle. Integration with AWS CloudTrail helps you track who is generating code signatures and to meet your compliance requirements.
+
+# References
+
+- https://docs.aws.amazon.com/signer/latest/developerguide/Welcome.html
--- lambda.tf
+++ lambda.tf
@@ -16,21 +16,75 @@ resource "aws_s3_bucket" "lambda_code_bucket" {
   bucket = "${var.function_name}-lambda-code-${var.environment}"
 }
 
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.lambda_code_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
 resource "aws_s3_object" "lambda_code" {
   bucket      = aws_s3_bucket.lambda_code_bucket.id
   key         = "${var.function_name}.zip"
   source      = local.zip_file_path
   source_hash = data.archive_file.lambda_zip.output_base64sha256
-  depends_on  = [data.archive_file.lambda_zip]
+  depends_on  = [data.archive_file.lambda_zip, aws_s3_bucket_versioning.versioning_example]
+}
+
+
+## Lambda Signer 
+
+resource "aws_signer_signing_profile" "tfsigner" {
+  name_prefix = "tfsigner"
+  platform_id = "AWSLambda-SHA384-ECDSA"
+}
+
+
+resource "aws_signer_signing_job" "this" {
+  profile_name = aws_signer_signing_profile.tfsigner.name
+
+  source {
+    s3 {
+      bucket  = aws_s3_bucket.lambda_code_bucket.id
+      key     = aws_s3_object.lambda_code.id
+      version = aws_s3_object.lambda_code.version_id
+    }
+  }
+
+  destination {
+    s3 {
+      bucket = aws_s3_bucket.lambda_code_bucket.id
+      prefix = "signed/"
+    }
+  }
+
+  ignore_signing_job_failure = true
+}
+
+resource "aws_lambda_code_signing_config" "tfsigner_code" {
+  allowed_publishers {
+    signing_profile_version_arns = [aws_signer_signing_profile.tfsigner.version_arn]
+  }
+  policies {
+    untrusted_artifact_on_deployment = "Enforce"
+  }
 }
 
 // Lambda
 resource "aws_lambda_function" "lambda" {
   function_name = var.function_name
-  s3_bucket     = aws_s3_bucket.lambda_code_bucket.id
-  s3_key        = "${var.function_name}.zip"
-  role          = aws_iam_role.lambda_role.arn
-  publish       = true
+  #s3_bucket     = aws_s3_bucket.lambda_code_bucket.id
+  #s3_key        = "${var.function_name}.zip"
+  role    = aws_iam_role.lambda_role.arn
+  publish = true
+  # Signer
+  code_signing_config_arn = aws_lambda_code_signing_config.tfsigner_code.arn
+
+  # S3 for signer configurations
+
+  s3_bucket = aws_signer_signing_job.this.signed_object[0].s3[0].bucket
+  s3_key    = aws_signer_signing_job.this.signed_object[0].s3[0].key
+
   # TODO
   # description      = "Upsert leads and individuals towards salesforce."
   # filename         = local.zip_file_path
